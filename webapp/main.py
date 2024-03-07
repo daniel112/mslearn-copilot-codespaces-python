80c184f6ba5a27d709cb39604ccfeab8f58a6e11
Daniel Yo <danielyo112@gmail.com>
2024-03-07 05:47:23 +0000

    checksum post method

--- webapp/main.py
+++ webapp/main.py
@@ -6,12 +6,17 @@ from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
+import hashlib
 
 current_dir = dirname(abspath(__file__))
 static_path = join(current_dir, "static")
 
 app = FastAPI()
 app.mount("/ui", StaticFiles(directory=static_path), name="ui")
+
+
+class Text(BaseModel):
+    text: str
 
 
 class Body(BaseModel):
@@ -35,3 +40,17 @@ def generate(body: Body):
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+
+@app.post('/checksum')
+def calculate_checksum(text: Text):
+    """
+    Calculate the checksum of the given text.
+
+    Example POST request body:
+    {
+        "text": "Hello, world!"
+    }
+    """
+    checksum = hashlib.sha256(text.text.encode()).hexdigest()
+    return {'checksum': checksum}
